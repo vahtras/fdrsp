bebb9ddff88534cd668053b290e97bf2062f588b
Olav Vahtras <vahtras@kth.se>
2013-12-17 17:31:53 +0100

    Use bootstrap style sheets for html table

--- makehtml.py
+++ makehtml.py
@@ -13,8 +13,34 @@ def short(log):
 header = ["Functional"] + ['<a href="%s">%s</a>'%(log, short(log)) for log in logfiles]
 
 htmlfile = open('test_findif.html', 'w')
-htmlfile.write(str(datetime.datetime.now()))
-htmlfile.write('<table border="1">\n')
+htmlfile.write('''\
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Finite differend tests</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <h1>Dalton testing</h1>
+    <h2>Finite field tests of DFT response functions</h2>
+''')
+with open('./test_findif_lr_closed_singlet.d/hf.out') as hfout:
+    for line in hfout:
+        if "Git" in line:
+            git_revision = line.split('|')[1]
+            break
+htmlfile.write('Git revision: %s<br>' % git_revision)
+htmlfile.write('<table class="table table-bordered table-hover table-condensed">\n')
 htmlfile.write('<tr><th>' + '</th><th>'.join(header) + '</th></tr>\n')
 for ijk in zip(*allfiles):
     spl = [ line.split('...')[-1].strip() for line in ijk ]
@@ -28,6 +54,15 @@ for ijk in zip(*allfiles):
             
     htmlfile.write('<tr><td>' + '</td><td>'.join(spl) + '</td></tr>\n')
 htmlfile.write('</table>')
-htmlfile.write('*A fraction of HF exchange was used to aid SCF convergence')
+htmlfile.write('*A fraction of HF exchange was used to aid SCF convergence<br>')
+htmlfile.write("Calculated at %s <br>" % str(datetime.datetime.now()))
+htmlfile.write('''
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
+''')
 htmlfile.close()
 
